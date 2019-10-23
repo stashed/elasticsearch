c0241d49aa0825b709c004b39ee377d60e2b2947
Tamal Saha <tamal@appscode.com>
2019-10-23 00:43:09 -0700

    Enable make ci (#12)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- pkg/restore.go
+++ pkg/restore.go
@@ -7,6 +7,10 @@ import (
 	"path/filepath"
 	"strings"
 
+	api_v1beta1 "stash.appscode.dev/stash/apis/stash/v1beta1"
+	"stash.appscode.dev/stash/pkg/restic"
+	"stash.appscode.dev/stash/pkg/util"
+
 	"github.com/appscode/go/flags"
 	"github.com/appscode/go/log"
 	"github.com/codeskyblue/go-sh"
@@ -15,9 +19,6 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	appcatalog_cs "kmodules.xyz/custom-resources/client/clientset/versioned"
-	api_v1beta1 "stash.appscode.dev/stash/apis/stash/v1beta1"
-	"stash.appscode.dev/stash/pkg/restic"
-	"stash.appscode.dev/stash/pkg/util"
 )
 
 func NewCmdRestore() *cobra.Command {
