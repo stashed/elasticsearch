6d120426e2ce5db98a1308fc5e229c133c5a52df
Md. Emruz Hossain <hossainemruz@gmail.com>
2020-06-07 02:19:15 +0600

    Make waitTimeout Configurable through flag (#22)
    Signed-off-by: hossainemruz <emruz@appscode.com>

--- apis/installer/v1alpha1/stash_elasticsearch_types.go
+++ apis/installer/v1alpha1/stash_elasticsearch_types.go
@@ -50,6 +50,7 @@ type StashElasticsearchSpec struct {
 	Image            ImageRef             `json:"image" protobuf:"bytes,3,opt,name=image"`
 	Backup           ElasticsearchBackup  `json:"backup" protobuf:"bytes,4,opt,name=backup"`
 	Restore          ElasticsearchRestore `json:"restore" protobuf:"bytes,5,opt,name=restore"`
+	WaitTimeout      int64                `json:"waitTimeout" protobuf:"varint,6,opt,name=waitTimeout"`
 }
 
 type ImageRef struct {
