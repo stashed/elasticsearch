0c4cd3b45e088b3464b5bba9e384d04b317a2986
Md. Emruz Hossain <hossainemruz@gmail.com>
2020-06-08 15:47:32 +0600

    Allow overwriting secret key via SecretTransformation (#23)
    Signed-off-by: hossainemruz <emruz@appscode.com>

--- pkg/restore.go
+++ pkg/restore.go
@@ -147,6 +147,12 @@ func (opt *esOptions) restoreElasticsearch() (*restic.RestoreOutput, error) {
 		return nil, err
 	}
 
+	// transform secret
+	err = appBinding.TransformSecret(opt.kubeClient, appBindingSecret.Data)
+	if err != nil {
+		return nil, err
+	}
+
 	// clear directory before running multielasticdump
 	log.Infoln("Cleaning up directory: ", opt.interimDataDir)
 	if err := clearDir(opt.interimDataDir); err != nil {
