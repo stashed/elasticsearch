f9f6ab1fc0267b48bc9fd49efb9a1facd6c73b04
lgtm <1gtm@users.noreply.github.com>
2020-08-12 04:06:55 -0700

    [cherry-pick] Fix output format + Add PreBackupActions logic (#131) (#133)
    Signed-off-by: hossainemruz <emruz@appscode.com>

--- pkg/restore.go
+++ pkg/restore.go
@@ -34,6 +34,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
+	appcatalog "kmodules.xyz/custom-resources/apis/appcatalog/v1alpha1"
 	appcatalog_cs "kmodules.xyz/custom-resources/client/clientset/versioned"
 	v1 "kmodules.xyz/offshoot-api/api/v1"
 )
@@ -75,15 +76,23 @@ func NewCmdRestore() *cobra.Command {
 				return err
 			}
 
+			targetRef := api_v1beta1.TargetRef{
+				APIVersion: appcatalog.SchemeGroupVersion.String(),
+				Kind:       appcatalog.ResourceKindApp,
+				Name:       opt.appBindingName,
+			}
 			var restoreOutput *restic.RestoreOutput
-			restoreOutput, err = opt.restoreElasticsearch()
+			restoreOutput, err = opt.restoreElasticsearch(targetRef)
 			if err != nil {
 				restoreOutput = &restic.RestoreOutput{
-					HostRestoreStats: []api_v1beta1.HostRestoreStats{
-						{
-							Hostname: opt.restoreOptions.Host,
-							Phase:    api_v1beta1.HostRestoreFailed,
-							Error:    err.Error(),
+					RestoreTargetStatus: api_v1beta1.RestoreMemberStatus{
+						Ref: targetRef,
+						Stats: []api_v1beta1.HostRestoreStats{
+							{
+								Hostname: opt.restoreOptions.Host,
+								Phase:    api_v1beta1.HostRestoreFailed,
+								Error:    err.Error(),
+							},
 						},
 					},
 				}
@@ -124,7 +133,7 @@ func NewCmdRestore() *cobra.Command {
 	return cmd
 }
 
-func (opt *esOptions) restoreElasticsearch() (*restic.RestoreOutput, error) {
+func (opt *esOptions) restoreElasticsearch(targetRef api_v1beta1.TargetRef) (*restic.RestoreOutput, error) {
 	// apply nice, ionice settings from env
 	var err error
 	opt.setupOptions.Nice, err = v1.NiceSettingsFromEnv()
@@ -183,7 +192,7 @@ func (opt *esOptions) restoreElasticsearch() (*restic.RestoreOutput, error) {
 	}
 
 	// Run restore
-	restoreOutput, err := resticWrapper.RunRestore(opt.restoreOptions)
+	restoreOutput, err := resticWrapper.RunRestore(opt.restoreOptions, targetRef)
 	if err != nil {
 		return nil, err
 	}
